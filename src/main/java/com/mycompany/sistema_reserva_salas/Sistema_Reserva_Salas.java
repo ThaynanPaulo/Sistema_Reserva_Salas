eeea1c23959ddd52b0916378a506eeec57d2ef01
Thaynan Paulo Fernandes Bezerra de Mendonça <Thaynan01@hotmail.com.br>
2025-01-19 14:45:58 -0300

    Add files via upload
    Criação classe usuários

---
+++ src/main/java/com/mycompany/sistema_reserva_salas/Sistema_Reserva_Salas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema_reserva_salas;
+
+/**
+ *
+ * @author Thaynan
+ */
+public class Sistema_Reserva_Salas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/sistema_reserva_salas/Usuario.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistema_reserva_salas;
+
+/**
+ *
+ * @author Thaynan
+ */
+public class Usuario {
+    private String nome;
+    private int matricula;
+    private String tipo_usuario; //discente, servidor, terceirizado;
+    
+    public Usuario( String nome, int matricula, String tipo) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.tipo_usuario = tipo;
+        
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the matricula
+     */
+    public int getMatricula() {
+        return matricula;
+    }
+
+    /**
+     * @param matricula the matricula to set
+     */
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    /**
+     * @return the tipo_usuario
+     */
+    public String getTipo_usuario() {
+        return tipo_usuario;
+    }
+
+    /**
+     * @param tipo_usuario the tipo_usuario to set
+     */
+    public void setTipo_usuario(String tipo_usuario) {
+        this.tipo_usuario = tipo_usuario;
+    }
+    
+    
+    
+}
